a353087aeb1c6ab204f928e2da8f6cf612824550
Abdurahmon <abduqodirovabdurakhmon@gmail.com>
2025-08-25 15:12:58 +0500

    last changes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+import Digital from '~/components/Digital.vue';
+import Hero from '~/components/Hero.vue';
+import Projects from '~/components/Projects.vue';
+import Services from '~/components/Services.vue';
+import Users from '~/components/Users.vue';
+
+</script>
 
 <template>
   <Hero />
